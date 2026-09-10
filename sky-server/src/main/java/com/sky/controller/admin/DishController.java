eb739bc6bd407299dbe120518ab39e838428fdb9
ht <220752710488@qq.com>
2026-09-10 15:01:27 +0800

    新增菜品业务

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.service.DishService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/dish")
+@Slf4j
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    /**
+     * 新增菜品
+     * @param dishDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增菜品")
+    public Result save(@RequestBody DishDTO dishDTO){
+        log.info("新增菜品：{}", dishDTO);
+        dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+}
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,15 @@
 package com.sky.mapper;
 
+import com.sky.entity.Dish;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
+
+
+    void insert(Dish dish) ;
 
     /**
      * 根据分类id查询菜品数量
