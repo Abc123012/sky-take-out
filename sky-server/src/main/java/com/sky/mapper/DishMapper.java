19d016710c8dfcfb4785462497645d3ea82e9df1
ht <220752710488@qq.com>
2026-09-12 14:40:26 +0800

    删除菜品业务

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,9 +1,14 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -19,4 +24,15 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteBatchIds(List<Long> ids);
+
+    /*
+    *
+    * 根据主键查询菜品
+    *
+    * */
+    @Select("select *from dish where id=#{id}")
+    Dish getById(Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,17 +1,30 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.sky.constant.MessageConstant.DISH_BE_RELATED_BY_SETMEAL;
+import static com.sky.constant.MessageConstant.DISH_ON_SALE;
 
 
 @Service
@@ -21,17 +34,20 @@ public class DishServiceImpl implements DishService {
     private DishMapper dishMapper;
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
-
-
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
     @Transactional
     @Override
     public void saveWithFlavor(DishDTO dishDTO) {
         Dish dish=new Dish();
         BeanUtils.copyProperties(dishDTO,dish);
-
-
         //向菜品表插入一条数据
         dishMapper.insert(dish);
+        //补充基础字段
+        dish.setUpdateTime(LocalDateTime.now());
+        dish.setUpdateUser(BaseContext.getCurrentId());
+        dish.setCreateTime(LocalDateTime.now());
+        dish.setCreateUser(BaseContext.getCurrentId());
         //获取插入后的菜品ID
         Long dishId = dish.getId();
 
@@ -40,10 +56,36 @@ public class DishServiceImpl implements DishService {
             flavors.forEach(flavor -> flavor.setDishId(dishId));
             //批量插入
             dishFlavorMapper.insertBatch(flavors);
-
-
         }
+    }
 
+    @Override
+    public PageResult page(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
+        return new PageResult(page.getTotal(), page.getResult());
 
     }
+
+    @Transactional
+    @Override
+    public void deleteBatch(List<Long> ids) {
+        //判断当前菜品是否能被删除
+        for (Long id : ids) {
+            Dish dish = dishMapper.getById(id);
+            if (dish.getStatus() == StatusConstant.ENABLE) {
+                throw new DeletionNotAllowedException(DISH_ON_SALE);
+            }
+        }
+        //判断当前菜品是否被套餐关联
+        List<Long> byDishIds = setmealDishMapper.getSetmealIdsByDishIds(ids);
+        if (byDishIds!=null && byDishIds.size()>0){
+            throw new DeletionNotAllowedException(DISH_BE_RELATED_BY_SETMEAL);
+        }
+        dishMapper.deleteBatchIds(ids);
+        dishFlavorMapper.deleteBatchIds(ids);
+
+    }
+
+
 }
